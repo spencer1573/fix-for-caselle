1b2735ce91712fe46cd9fad93831b773a93f3277
spencer1573 <spencer1573@users.noreply.github.com>
2021-08-02 17:27:10 -0700

    setting object fit to cover for 4 images

--- src/app.js
+++ src/app.js
@@ -1,14 +1,5 @@
-// const playwright = require('playwright')(
 import playwright from 'playwright'
-
-// here
 ;(async () => {
-  // for (const browserType of ['chromium', 'firefox', 'webkit']) {
-  // TODO #keep for later
-  // args: ['--start-fullscreen', '--window-size=1896,896'],
-  // viewport: { width: 1896, height: 720 },
-
-  console.log('something')
   const browser = await playwright['chromium'].launch({
     headless: false,
   })
@@ -16,7 +7,20 @@ import playwright from 'playwright'
   const page = await context.newPage()
   await page.setViewportSize({ width: 1920, height: 1080 })
   await page.goto('https://www.caselle.com#events')
-  // await page.screenshot({ path: `example.png` })
-  // await browser.close()
-  // }
+  // wait for image elements to exist
+  await page.waitForSelector('#events img')
+  await page.evaluate((_) => {
+    const eventsElement = document.getElementById('events')
+
+    const imagesToAdjustRaw = eventsElement.getElementsByTagName('img')
+
+    const imagesToAdjust = Array.from(imagesToAdjustRaw)
+
+    // take out that last image that is square
+    imagesToAdjust.pop()
+
+    imagesToAdjust.forEach((image, index) => {
+      image.style.objectFit = 'cover'
+    })
+  })
 })()
